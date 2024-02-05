5108f332076079e1226745a06d9b77329324502e
Lamiaa21-3 <lamiaagamal213@gmail.com>
2024-02-05 10:45:48 +0200

    add onboarding view

---
+++ lib/pages/onboarding/onboarding_view.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class OnBoardingView extends StatelessWidget {
+  const OnBoardingView({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+    
+  }
+}
